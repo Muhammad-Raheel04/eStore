6018f21f26d9a398c50987eae27d24b08892e61a
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-12 18:24:29 +0500

    added basic server setup

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import 'dotenv/config';
+import connectDB from './database/db.js';
+
+const app=express();
+
+const PORT=process.env.PORT;
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server is listening at port ${PORT}`);
+
+})
