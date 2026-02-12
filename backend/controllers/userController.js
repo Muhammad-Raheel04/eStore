d033c3f15815ba477ae80a891332d7a2c4d70959
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-12 20:57:11 +0500

    implemented email verification with JWT token validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 import { User } from "../models/userModel.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
     try {
@@ -23,6 +25,9 @@ export const register = async (req, res) => {
             email,
             password: hashedPassword
         })
+        const token = jwt.sign({ id: newUser._id }, process.env.SECRET_KEY, { expiresIn: '10m' });
+        verifyEmail(token, email); //send email here
+        newUser.token = token
         await newUser.save();
         return res.status(201).json({
             success: true,
@@ -33,6 +38,53 @@ export const register = async (req, res) => {
         res.status(500).json({
             success: false,
             message: error.message
+        })
+    }
+}
+
+export const verify = async (req, res) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            res.status(400).json({
+                success: false,
+                message: `Authorization token is missing or invalid`
+            })
+        }
+        const token = authHeader.split(' ')[1];
+        let decoded;
+        try {
+            decoded = jwt.verify(token, process.env.SECRET_KEY);
+        } catch (error) {
+            if(error.name ==="TokenExpiredError"){
+                return res.status(400).json({
+                    success:false,
+                    message:"The registration token has expired"
+                })
+            }
+            return res.status(400).json({
+                success:false,
+                message:"Token verification failed"
+            })
+        }
+        const user=await User.findById(decoded.id);
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        user.token=null
+        user.isVerified=true;
+        await user.save();
+        return res.status(200).json({
+            success:true,
+            message:"Email verified successfully"
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            message:error.message
         })
     }
 }
