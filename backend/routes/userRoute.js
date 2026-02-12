38a997cc1b6442d5150682c1c9a1d2fa33ad0432
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-12 21:36:59 +0500

    added verify and reverify post routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import { register } from '../controllers/userController.js';
-
+import { verify } from 'jsonwebtoken';
+import {verify} from '../controllers/userController.js';
+import {reVerify} from '../controllers/userController.js';
 const router =express.Router();
 
 router.post("/register",register);
+router.post("/verify",verify);
+router.post("/reverify",reVerify)
 
 export default router;
