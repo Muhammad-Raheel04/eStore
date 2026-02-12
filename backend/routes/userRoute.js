a573ff06e2396e707e3d1714851c068351fa8792
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-13 04:54:30 +0500

    imported route handlers in a single import removed multiple imports

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { register } from '../controllers/userController.js';
+import { register,login ,reVerify,verify} from '../controllers/userController.js';
 import { verify } from 'jsonwebtoken';
-import {verify} from '../controllers/userController.js';
-import {reVerify} from '../controllers/userController.js';
+
 const router =express.Router();
 
 router.post("/register",register);
 router.post("/verify",verify);
-router.post("/reverify",reVerify)
+router.post("/reverify",reVerify);
+router.post('/login',login);
 
 export default router;
