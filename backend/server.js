8e8f742f9c27c2301f098b5eeac0c410bd7648c1
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-12 20:22:55 +0500

    added express json middleware and api route for user

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 import connectDB from './database/db.js';
+import userRoute from './routes/userRoute.js';
 
 const app=express();
 
+app.use('/api/v1/user',userRoute);
+app.use(express.json());
 const PORT=process.env.PORT;
 app.listen(PORT,()=>{
     connectDB();
