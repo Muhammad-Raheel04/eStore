3015a24be4eb6c22afdd03fcaa64603dac93fda3
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-13 06:24:22 +0500

    added mistakenly removed imports from userController

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,4 +1,5 @@
-import express from 'express'
+import express from 'express';
+import { register,login ,reVerify,verify,logout} from '../controllers/userController.js';
 import { isAuthenticated } from '../middleware/isAuthenticated.js';
 
 const router =express.Router();
