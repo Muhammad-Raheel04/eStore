a4e5ea7b61de94deb3b838e568744a2207887cf8
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-13 05:28:01 +0500

    added jwt verification and user validation in authentication middleware

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,3 +1,5 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel.js";
 
 
 export const isAuthenticated = async (req,res,next)=>{
@@ -9,6 +11,32 @@ export const isAuthenticated = async (req,res,next)=>{
                 message:"Authorization token is missing or invalid"
             })
         }
+        const token =authHeader.split(' ')[1];
+        let decoded;
+        try{
+            decoded=jwt.verify(token,process.env.SECRET_KEY);
+        }catch(error){
+            if(error.name==="TokenExpiredError"){
+                return res.status(400).json({
+                    success:false,
+                    message:"The registration token has expired"
+                })
+            }
+            return res.status(400).json({
+                success:false,
+                message:"Access token is missing or invalid"
+            })
+        }
+
+        const user=await User.findById(decoded.id);
+        if(!user){
+            return res.status(400).json({
+                success:false,
+                messsage:"User not found"
+            })
+        }
+        req.id=user._id;
+        next();
     }catch(error){
         return res.status(500).json({
             success:true,
