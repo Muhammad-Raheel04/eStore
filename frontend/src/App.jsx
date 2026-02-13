77e3e0081b969cca6e7ca8ec263ad280d6dcbca9
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-13 16:31:59 +0500

    basic frontend project setup for eStore

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button";
+const App = () => {
+  return (
+    <div className='text-red-700'>
+      Hello World
+      <Button>Click Me</Button>
+      <button>Click Me</button>
+    </div>
+  )
+}
+
+export default App
