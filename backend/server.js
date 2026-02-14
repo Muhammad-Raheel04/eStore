570fd52dd5d07734f85270bd3ce480a89b2df1ed
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 05:17:00 +0500

    Enable CORS middleware to allow frontend-backend communication

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import 'dotenv/config';
 import connectDB from './database/db.js';
 import userRoute from './routes/userRoute.js';
-
+import cors from 'cors';
 const app=express();
+app.use(cors());
 app.use(express.json());
 app.use('/api/v1/user',userRoute);
 
