46bb0df7bdaf524ccec7c77088ef5f579ee48747
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 05:40:04 +0500

    added health check route

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,17 +1,18 @@
 import express from 'express';
-import { register,login ,reVerify,verify,logout, forgotPassword, verifyOTP, changePassword,allUser, getUserById} from '../controllers/userController.js';
+import { healthCheck,register,login ,reVerify,verify,logout, forgotPassword, verifyOTP, changePassword,allUser, getUserById} from '../controllers/userController.js';
 import { isAuthenticated,isAdmin } from '../middleware/isAuthenticated.js';
 
 const router =express.Router();
 
+router.get('/',healthCheck)
 router.post("/register",register);
 router.post("/verify",verify);
 router.post("/reverify",reVerify);
 router.post('/login',login);
 router.post("/logout",isAuthenticated,logout);
-router.post('/forgot-password',isAuthenticated,forgotPassword);
+router.post('/forgot-password',forgotPassword);
 router.post('/verify-otp/:email',verifyOTP);
-router.post('/change-password/:email',changePassword);
+router.post('/change-password/:email',isAuthenticated,changePassword);
 router.get('/all-user',isAdmin,allUser);
 router.get('/get-user/:userId',getUserById)
 export default router;
