70c6ba139c0bc0a02f3499db6a29cd0274a0f0b4
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 07:21:45 +0500

    fixed typo in message in json response

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -32,7 +32,7 @@ export const isAuthenticated = async (req,res,next)=>{
         if(!user){
             return res.status(400).json({
                 success:false,
-                meswhasage:"User not found"
+                message:"User not found"
             })
         }
         req.user=user;
