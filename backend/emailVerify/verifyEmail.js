27de42b1de8d3611bd327ad73433fa24440f1573
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 17:15:08 +0500

    updated host value of transporter

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -4,7 +4,7 @@ export const verifyEmail = async (token, email) => {
     try {
         const transporter = nodemailer.createTransport(
             {
-                host:'smtp.mailgun.org',
+                host:'smtp-relay.brevo.org',
                 port:587,
                 secure:false,
                 auth: {
