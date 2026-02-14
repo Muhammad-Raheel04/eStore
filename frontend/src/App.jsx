ab88a463c93649e97803fb5dc9fb69bbee509faf
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 18:52:57 +0500

    added verify email forgot-password and verify-otp:email routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Button } from "@/components/ui/button";
+import Features from './components/Features';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from "./pages/Home";
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Verify from './pages/Verify';
+import ForgotPassword from './pages/ForgotPassword';
+import VerifyOTP from './pages/VerifyOTP';
+import ChangePassword from './pages/ChangePassword';
+import VerifyEmail from './pages/VerifyEmail';
+import Footer from './components/Footer';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <><Navbar /><Home /></>
+    element: <><Navbar /><Home/><Footer/></>
   },
   {
     path: '/signup',
@@ -26,7 +32,19 @@ const router = createBrowserRouter([
   },
   {
     path: '/verify:token',
-    element: <><Verify /></>
+    element: <><VerifyEmail /></>
+  },
+  {
+    path: '/forgot-password',
+    element: <><ForgotPassword /></>
+  },
+  {
+    path: '/verify-otp/:email',
+    element: <><VerifyOTP /></>
+  },
+  {
+    path: '/change-password/:email',
+    element: <><ChangePassword /></>
   },
 ])
 const App = () => {
