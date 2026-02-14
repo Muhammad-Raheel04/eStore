f65993d890825c612136b8d0da26ebd6b2964952
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 18:51:18 +0500

    added Hero and Features components

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Hero from '@/components/Hero'
+import Features from '@/components/Features'
 const Home = () => {
   return (
     <div>
-      Home
+      <Hero/>
+      <Features/>
     </div>
   )
 }
