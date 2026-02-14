80daefb53ddd42461d11d4982b3702aeec582e49
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 21:55:26 +0500

    wrap App with Provider and PersistGate for state persistence

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,37 +4,59 @@ import { Button } from "@/components/ui/button";
 import API from "@/utils/API";
 import { toast } from "sonner";
 import { ShoppingCart } from 'lucide-react';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '@/redux/userSlice';
 const Navbar = () => {
-  const user=false;
+  const { user } = useSelector(store => store.user)
+  const accessToken = localStorage.getItem('accessToken');
+  const dispatch = useDispatch();
+  const logoutHandler = async () => {
+    try {
+      const res = await API.post('user/logout', {}, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      if (res.data.success) {
+        localStorage.removeItem('accessToken');
+        dispatch(setUser(null));
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      console.log();
 
-
+    }
+  }
   return (
     <header className="bg-pink-50 fixed w-full z-20 border-b border-pink-200">
       <div className='max-w-7xl mx-auto flex justify-between items-center py-3'>
-         {/* {logo section} */}
-         <div >
-          <img src='/eStore.svg' alt='' className='w-[100px]'/>
-         </div>
-         {/* {nav section} */}
-         <nav className='flex gap-10 justify-between items-center'>
+        {/* {logo section} */}
+        <div >
+          <img src='/eStore.svg' alt='' className='w-[100px]' />
+        </div>
+        {/* {nav section} */}
+        <nav className='flex gap-10 justify-between items-center'>
           <ul className='flex gap-7 items-center text-xl font-semibold'>
             <Link to={'/'}><li>Home</li></Link>
             <Link to={'/products'}><li>Products</li></Link>
             {
-              user &&   <Link to={'/profile'}><li>Products</li></Link>
+              user && <Link to={'/profile'}><li>Hello, {user.firstName}</li></Link>
             }
           </ul>
           <Link to={'/cart'} className='relative'>
-          <ShoppingCart>
-            <span className='bg-pink-500 rounded-full absolute text-white -top-3 -right-5 px-2'>0</span>
-          </ShoppingCart>
+            <ShoppingCart>
+              <span className='bg-pink-500 rounded-full absolute text-white -top-3 -right-5 px-2'>0</span>
+            </ShoppingCart>
           </Link>
           {
-            user? <Button className='bg-pink-600 text-white cursor-'>Logout</Button>:
-                  <Button className='bg-gradient-to-tl from-blue-600 to-purple-600 text-white cursor-'>Login</Button>
+            user ? <Button onClick={logoutHandler} className='bg-pink-600 text-white cursor-'>Logout</Button> :
+              <Link to="/login">
+                <Button className='bg-gradient-to-tl from-blue-600 to-purple-600 text-white px-4 py-2 rounded-md hover:opacity-90'>
+                  Login
+                </Button>
+              </Link>
           }
-         </nav>
+        </nav>
       </div>
     </header>
   );
