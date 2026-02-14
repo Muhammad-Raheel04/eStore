4465cdcc09f8ef819517fcae3059d20eda94d8eb
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-14 09:43:44 +0500

    updated Ndemailer transporter to use Gmail TLS for email sending

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -1,35 +1,36 @@
 import nodemailer from 'nodemailer'
 
 export const verifyEmail = async (token, email) => {
-    try{
-    const transporter = nodemailer.createTransport(
-        {
-            service: 'gmail',
-            auth: {
-                user: process.env.MAIL_USER,
-                pass: process.env.MAIL_PASS
+    try {
+        const transporter = nodemailer.createTransport(
+            {
+                host: "smtp.gmail.com",
+                port: 587,
+                secure: false,
+                auth: {
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS
+                }
             }
-        }
-    );
-    const mailDetails = {
-        from: process.env.MAIL_USER,
-        to: email,
-        subject: 'Email Verification',
-        html: `
+        );
+        const mailDetails = {
+            from: process.env.MAIL_USER,
+            to: email,
+            subject: 'Email Verification',
+            html: `
                 <p>Hi!</p>
                 <p>You recently registered on our website.</p>
                 <p>Click the link below to verify your email:</p>
                 <a href="https://estore-production-b778.up.railway.app/api/v1/user/verify/${token}">Verify Email</a>
                 <p>Thanks!</p>
             `
-    };
-    const info=await transporter.sendMail(mailDetails);
-    console.log("Email sent successfully");
-    return true;
-}catch(error){
-    console.log("Error sending email",error);
-    throw new Error("Email could not be sent");
-}
+        };
+        const info = await transporter.sendMail(mailDetails);
+        console.log("Email sent successfully");
+        return true;
+    } catch (error) {
+        console.log("Error sending email", error);
+    }
 }
 
 
