a99fdc1870acc349b3264985f9e34691b437b2fb
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-15 07:15:10 +0500

    separated get and post routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -4,15 +4,16 @@ import { isAuthenticated,isAdmin } from '../middleware/isAuthenticated.js';
 
 const router =express.Router();
 
-router.get('/',healthCheck)
+
 router.post("/register",register);
-router.post("/verify",verify);
 router.post("/reverify",reVerify);
 router.post('/login',login);
 router.post("/logout",isAuthenticated,logout);
 router.post('/forgot-password',forgotPassword);
 router.post('/verify-otp/:email',verifyOTP);
 router.post('/change-password/:email',isAuthenticated,changePassword);
+router.post("/verify",verify);
 router.get('/all-user',isAdmin,allUser);
+router.get('/',healthCheck)
 router.get('/get-user/:userId',getUserById)
 export default router;
