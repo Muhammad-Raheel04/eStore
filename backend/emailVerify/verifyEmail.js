9c32e6d09a901c3300be758ef3824bed5712b470
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-15 07:14:28 +0500

    fixed href value of anchor tag in verification email and updated from key in mailDetails to EMAIL_FROM after validating it on brevo

--- backend/emailVerify/verifyEmail.js
+++ backend/emailVerify/verifyEmail.js
@@ -4,24 +4,26 @@ export const verifyEmail = async (token, email) => {
     try {
         const transporter = nodemailer.createTransport(
             {
-                host:'smtp-relay.brevo.com',
-                port:587,
-                secure:false,
+                host: 'smtp-relay.brevo.com',
+                port: 587,
+                secure: false,
                 auth: {
                     user: process.env.BREVO_USER,
                     pass: process.env.BREVO_PASS
                 }
             }
         );
+        await transporter.verify();
+        console.log("SMTP connection successful");
         const mailDetails = {
-            from: process.env.BREVO_USER,
+            from:process.env.EMAIL_FROM,
             to: email,
             subject: 'Email Verification',
             html: `
                 <p>Hi!</p>
                 <p>You recently registered on our website.</p>
                 <p>Click the link below to verify your email:</p>
-                <a href="https://estore-production-b778.up.railway.app/api/v1/user/verify/${token}">Verify Email</a>
+                <a target="_self" href="${process.env.FRONTEND_URL}/verify/${token}">Verify Email</a>
                 <p>Thanks!</p>
             `
         };
