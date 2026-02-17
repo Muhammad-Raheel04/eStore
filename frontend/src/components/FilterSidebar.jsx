b53cb8604946d1418901b0cf53bb6f467e2ea2da
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-17 22:14:36 +0500

    added FilterSidebar component with category, brand, and price range filters

--- frontend/src/components/FilterSidebar.jsx
+++ frontend/src/components/FilterSidebar.jsx
@@ -1,11 +1,96 @@
 import React from 'react'
+import { Input } from './ui/input';
+import { Label } from './ui/label';
+import { Button } from './ui/button';
 
-const FilterSlidebar = () => {
+const FilterSidebar = ({ search, setSearch, category, setCategory, brand, setBrand, setPriceRange, allProducts, priceRange }) => {
+  const Categories = allProducts.map(p => p.category)
+  const UniqueCategory = ["All", ...new Set(Categories)];
+  const Brands = allProducts.map(p => p.brand)
+  const UniqueBrand = ["All", ...new Set(Brands)];
+  console.log(UniqueBrand);
+
+  const handleCategoryClick = (val) => {
+    setCategory(val)
+  }
+
+  const handleBrandChange = (e) => {
+    setBrand(e.target.value);
+  }
+  const handleMinChange = (e) => {
+    const value = Number(e.target.value);
+    if (value <= priceRange[1]) setPriceRange([value, priceRange[1]])
+  }
+
+  const handleMaxChange = (e) => {
+    const value = Number(e.target.value);
+    if (value >= priceRange[0]) setPriceRange([priceRange[0], value])
+  }
+
+  const resetFilters = () => {
+    setSearch("");
+    setCategory("All");
+    setBrand("All");
+    setPriceRange(1000000)
+  }
   return (
-    <div>
-      SlideBar
+    <div className="bg-gray-100 mt-10 p-4 rounded-md h-max hidden md:block w-64">
+      {/* Search */}
+      <Input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search..."
+        className="bg-white p-2 rounded-md border-gray-400 border-2 w-full" />
+
+      {/* Category */}
+      <h1 className="mt-5 font-semibold text-xl">Category</h1>
+      <div className="flex flex-col gap-2 mt-3">
+        {UniqueCategory.map((item, index) => (
+          <div key={index} className="flex items-center gap-2">
+            <input
+              type="radio"
+              name="category"
+              checked={category === item}
+              onChange={() => handleCategoryClick(item)}
+              id={`category-${index}`} />
+            <Label htmlFor={`category-${index}`}>{item}</Label>
+          </div>
+        ))}
+      </div>
+
+      {/* Brands */}
+      <h1 className="mt-5 font-semibold text-xl">Brand</h1>
+      <select
+        className="bg-white w-full p-2 border border-gray-400 rounded-md mt-3"
+        value={brand}
+        onChange={handleBrandChange}
+      >
+        {UniqueBrand.map((item, index) => (
+          <option key={index} value={item}>
+            {item.toUpperCase()}
+          </option>
+        ))}
+      </select>
+      {/* price range */}
+      <h1 className='mt-5 font-semibold text-xl mb-3'>Price Range</h1>
+      <div className='flex flex-col gap-2'>
+        <label>
+          Price Range: Rs{priceRange[0]} - {priceRange[1]}
+        </label>
+        <div className="flex gap-2 items-center">
+          <input type="number" min="0" max="5000" value={priceRange[0]} onChange={handleMinChange} className="w-20 p-1 border border-gray-300 rounded" />
+          <input type="number" min="0" max="999999" value={priceRange[1]} onChange={handleMaxChange} className="w-20 p-1 border border-gray-300 rounded" />
+        </div>
+        <div>
+          <input type="range" min="0" value={priceRange[0]} onChange={handleMinChange} max="5000" step="100" className='w-full' />
+          <input type="range" min="0" value={priceRange[1]} onChange={handleMaxChange} max="999999" step="100" className='w-full' />
+        </div>
+        {/* Reset button */}
+        <Button onChange={resetFilters}className="bg-pink-600 text-white mt-5 cursor-pointer w-full" >Reset Filter</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default FilterSlidebar
+export default FilterSidebar;
