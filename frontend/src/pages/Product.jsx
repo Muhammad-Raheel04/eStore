f2452a3c8c946d6a6e27b30febb7d62b464c67f5
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-17 22:34:59 +0500

    implemented  product filtering, sorting, and Redux integration in Products page

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -10,39 +10,85 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
-import { useState ,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { toast } from 'sonner';
 import API from '@/utils/API';
+import { useDispatch, useSelector } from 'react-redux';
+import { setProducts } from '@/redux/productSlice';
 const Products = () => {
+    const { products } = useSelector(store => store.product);
     const [allProducts, setAllProducts] = useState([]);
-    const [loading,setLoading]=useState(false);
+    const [loading, setLoading] = useState(false);
+    const [search, setSearch] = useState("");
+    const [category, setCategory] = useState("All");
+    const [brand, setBrand] = useState("All");
+    const [priceRange, setPriceRange] = useState([0, 999999]);
+    const [sortOrder, setSortOrder] = useState('')
+    const dispatch = useDispatch();
     const getAllProducts = async () => {
         try {
             setLoading(true);
             const res = await API.get(`product/getallproducts`);
             if (res.data.success) {
                 setAllProducts(res.data.products);
+                dispatch(setProducts(res.data.products));
             }
         } catch (error) {
             console.log(error);
             toast.error(error.response.data.message);
-        }finally{
+        } finally {
             setLoading(false);
         }
     };
+    useEffect(() => {
+        if (allProducts.length === 0) return;
 
+        let filtered = [...allProducts];
+
+        if (search.trim() !== "") {
+            filtered = filtered.filter(p => p.productName?.toLowerCase().includes(search.toLowerCase()));
+        }
+
+        if (category !== "All") {
+            filtered = filtered.filter(p => p.category === category);
+        }
+
+        if (brand !== "All") {
+            filtered = filtered.filter(p => p.brand === brand);
+        }
+
+        filtered = filtered.filter(p => p.productPrice >= priceRange[0] && p.productPrice <= priceRange[1]);
+        if (sortOrder === "lowToHigh") {
+            filtered.sort((a, b) => a.productPrice - b.productPrice)
+        } else if (sortOrder === "highToLow") {
+            filtered.sort((a, b) => b.productPrice - a.productPrice)
+        }
+
+        dispatch(setProducts(filtered))
+    }, [sortOrder,allProducts, search, category, brand, priceRange,dispatch])
     useEffect(() => {
         getAllProducts();
     }, []);
+
     return (
         <div className='pt-20 pb-10'>
             <div className='max-w-7xl mx-auto flex gap-7'>
                 {/* sidebar */}
-                <FilterSidebar allProducts={allProducts}/>
+                <FilterSidebar
+                    search={search}
+                    setSearch={setSearch}
+                    brand={brand}
+                    setBrand={setBrand}
+                    category={category}
+                    setCategory={setCategory}
+                    allProducts={allProducts}
+                    priceRange={priceRange}
+                    setPriceRange={setPriceRange}
+                />
                 {/* Main product section */}
                 <div className="flex flex-col flex-1">
                     <div className='flex justify-end mb-4'>
-                        <Select>
+                        <Select onValueChange={(value)=>setSortOrder(value)}>
                             <SelectTrigger className="w-[180px]">
                                 <SelectValue placeholder="Sort by price" />
                             </SelectTrigger>
@@ -56,8 +102,8 @@ const Products = () => {
                     </div>
                     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-7'>
                         {
-                            allProducts.map((product) => {
-                                return <ProductCard key={product._id} product={product} loading={loading}/>;
+                            products.map((product) => {
+                                return <ProductCard key={product._id} product={product} loading={loading} />;
                             })
                         }
                     </div>
