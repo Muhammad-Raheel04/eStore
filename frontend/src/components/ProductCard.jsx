9eae57b05d6a38a711d7588240bdec24160e5026
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-18 19:34:18 +0500

    integrated add-to-cart functionality in ProductCard with API, Redux, and toast notifications

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -2,23 +2,46 @@ import { ShoppingCart } from 'lucide-react'
 import React from 'react'
 import { Button } from './ui/button'
 import { Skeleton } from './ui/skeleton'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { setCart } from '@/redux/productSlice'
+import API from '@/utils/API'
+import { toast } from 'sonner'
 
 const ProductCard = ({ product, loading }) => {
-  const { productImg, productPrice, productName } = product
+  const { productImg, productPrice, productName } = product;
+  const accessToken = localStorage.getItem("accessToken");
+  const dispatch=useDispatch();
+  const navigate =useNavigate();
+  const addToCart = async (productId) => {
+    try {
+      const res = await API.post('cart/add',{productId},{
+        headers:{
+          Authorization:`Bearer ${accessToken}`
+        }
+      });
+      if(res.data.success){
+        toast.success("Product added to Cart");
+        dispatch(setCart(res.data.cart));
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
   return (
     <div className='shadow-lg rounded-lg overflow-hidden h-max'>
       <div className='w-full h-full aspect-square overflow-hidden'>
         {loading ? (
           <Skeleton className='w-full h-full rounded-lg' />
         ) : (
-          <img 
-            src={productImg[0]?.url} 
-            alt={productName} 
-            className='w-full h-full transition-transform duration-300 hover:scale-105 cursor-pointer' 
+          <img
+            src={productImg[0]?.url}
+            alt={productName}
+            className='w-full h-full transition-transform duration-300 hover:scale-105 cursor-pointer'
           />
         )}
       </div>
-      
+
       {loading ? (
         <div className='px-2 space-y-2 my-2'>
           <Skeleton className='w-[200px] h-4' />
@@ -29,7 +52,7 @@ const ProductCard = ({ product, loading }) => {
         <div className='px-2 space-y-1'>
           <h1 className='font-semibold h-12 line-clamp-2'>{productName}</h1>
           <h2 className='font-bold'>${productPrice}</h2>
-          <Button className='bg-pink-600 mb-3 w-full'>
+          <Button onClick={()=>addToCart(product._id)} className='bg-pink-600 mb-3 w-full'>
             <ShoppingCart className="mr-2 h-4 w-4" />
             Add to Cart
           </Button>
