da44f752df3cb51eb8b66b121bd156619e2a191c
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-18 19:35:26 +0500

    added setCart reducer to productsSlice for updating cart state in Redux

--- frontend/src/redux/productSlice.js
+++ frontend/src/redux/productSlice.js
@@ -3,15 +3,22 @@ import { createSlice } from "@reduxjs/toolkit";
 const productsSlice = createSlice({
     name: 'product',
     initialState: {
-        products: []
+        products: [],
+        cart: {
+            items:[],
+            totalPrice:0
+        },
     },
     reducers: {
         // actions
         setProducts: (state, action) => {
             state.products = action.payload
+        },
+        setCart: (state, action) => {
+            state.cart = action.payload;
         }
     }
 })
 
-export const { setProducts } = productsSlice.actions
+export const { setProducts,setCart } = productsSlice.actions
 export default productsSlice.reducer
