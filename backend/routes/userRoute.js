44c7a68e19856d75e0223376e0e2c643ac49f407
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-23 06:46:41 +0500

    fixed /all-user route by adding isAuthenticated middleware before isAdmin middleware to correctly set user role and other necessary credentials

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -14,7 +14,7 @@ router.post('/forgot-password',forgotPassword);
 router.post('/verify-otp/:email',verifyOTP);
 router.post('/change-password/:email',isAuthenticated,changePassword);
 router.post("/verify",verify);
-router.get('/all-user',isAdmin,allUser);
+router.get('/all-user',isAuthenticated,isAdmin,allUser);
 router.get('/',healthCheck)
 router.get('/get-user/:userId',getUserById)
 router.put('/update/:id',isAuthenticated,singleUpload,updateUser);
