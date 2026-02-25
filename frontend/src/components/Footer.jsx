5f761a4725ab16013e993c39cbde54fe595d59a9
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-25 06:49:55 +0500

    removed some unnessary features from footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,10 +8,8 @@ const Footer = () => {
       <div className='max-w-7xl mx-auto px-4 md:flex md:justify-between'>
         {/* info */}
         <div className='mb-6 md:mb-0'>
-          <Link to='/'>
-            <img src='/eStore.svg' alt="EKart Logo" className='w-32' />
-          </Link>
-          <p className='mt-2 text-sm'>Powering Your World with the Best in Electronics.</p>
+       
+          <p className='mt-2 text-sm'>Powering Your World with the Best in eCommerce.</p>
           <p className='mt-2 text-sm'>123 Electronics St, Style City, NY 10001</p>
           <p className='text-sm'>Email: support@Zaptro.com</p>
           <p className='text-sm'>Phone: (123) 456-7890</p>
@@ -39,27 +37,11 @@ const Footer = () => {
             <FaTwitterSquare className='text-2xl hover:text-pink-600 cursor-pointer' />
           </div>
         </div>
-
-        {/* newsletter subscription */}
-        <div>
-          <h3 className='text-xl font-semibold'>Stay in the Loop</h3>
-          <p className='mt-2 text-sm'>Subscribe to get special offers, free giveaways, and more</p>
-          <form className='mt-4 flex'>
-            <input
-              type="email"
-              placeholder='Your email address'
-              className="w-full p-2 rounded-l-md text-white placeholder-white border border-white focus:outline-none focus:ring-2 focus:ring-gray-500"
-            />
-            <button type='submit' className='bg-pink-600 text-white px-4 rounded-r-md hover:bg-red-700'>
-              Subscribe
-            </button>
-          </form>
-        </div>
       </div>
 
       {/* bottom section */}
       <div className='mt-8 border-t border-gray-700 pt-6 text-center text-sm'>
-        <p>&copy; {new Date().getFullYear()} <span className='text-pink-600'>EKart</span>. All rights reserved</p>
+        <p>&copy; {new Date().getFullYear()} <span className='text-pink-600'>Hamza Rajpoot</span>. All rights reserved</p>
       </div>
     </footer>
   );
