a211bdd23bbef40ba04756cd844c9be56ac1766c
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-25 07:12:45 +0500

    added resposivenes to single product page for better UI

--- frontend/src/pages/SingleProduct.jsx
+++ frontend/src/pages/SingleProduct.jsx
@@ -10,12 +10,27 @@ const SingleProduct = () => {
     const productId = params.id
     const products = useSelector(store => store.product.products)
     const product = products.find(item => item._id === productId)
+
     return (
-        <div className="pt-20 py-10 max-w-7xl mx-auto">
+        <div className="pt-20 pb-10 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+            
             <Breadcrumbs product={product} />
-            <div className="mt-10 grid grid-cols-2 items-start">
-                <ProductImg images={product.productImg}/>
-                <ProductDesc product={product}/>
+
+            <div className="
+                mt-8
+                grid 
+                grid-cols-1 
+                md:grid-cols-2 
+                gap-8 
+                items-start
+            ">
+                <div className="w-full">
+                    <ProductImg images={product?.productImg} />
+                </div>
+
+                <div className="w-full">
+                    <ProductDesc product={product} />
+                </div>
             </div>
         </div>
     )
