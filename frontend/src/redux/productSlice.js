e24ac88049e7ee8e90167420f5ad8444abf0cbf3
Muhammad Raheel <raheel31102004@gmail.com>
2026-02-25 07:15:36 +0500

    added address management and clear cart actions

--- frontend/src/redux/productSlice.js
+++ frontend/src/redux/productSlice.js
@@ -5,9 +5,11 @@ const productsSlice = createSlice({
     initialState: {
         products: [],
         cart: {
-            items:[],
-            totalPrice:0
+            items: [],
+            totalPrice: 0
         },
+        addresses: [],
+        selectedAddress: null // currently chosen address
     },
     reducers: {
         // actions
@@ -16,9 +18,33 @@ const productsSlice = createSlice({
         },
         setCart: (state, action) => {
             state.cart = action.payload;
+        },
+        // Address Management
+        addAddress: (state, action) => {
+            if (!state.addresses) state.addresses = [];
+            state.addresses.push(action.payload);
+        },
+
+        setSelectedAddress: (state, action) => {
+            state.selectedAddress = action.payload;
+        },
+
+        deleteAddress: (state, action) => {
+            state.addresses = state.addresses.filter((_, index) => index !== action.payload);
+
+            // Reset selectedAdddress if it was deleted
+            if (state.selectedAddress === action.payload) {
+                state.selectedAddress = null;
+            }
+        },
+        clearCart: (state) => {
+            state.cart = {
+                items: [],
+                totalPrice: 0
+            };
         }
     }
 })
 
-export const { setProducts,setCart } = productsSlice.actions
+export const { setProducts, setCart, addAddress, setSelectedAddress, deleteAddress ,clearCart} = productsSlice.actions
 export default productsSlice.reducer
