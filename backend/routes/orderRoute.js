c39dc0ce55dc4bc240aec3bdbcd32f4ce231e631
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-02 19:36:07 +0500

    added identifyCart middleware for COD orders

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middleware/isAuthenticated.js";
 import { createCODOrder, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
+import { identifyCart } from "../middleware/cartIdentity.js";
 const router = express.Router();
 
 router.get('/all-orders',isAuthenticated,isAdmin,getAllOrders);
-router.post("/cod", isAuthenticated, createCODOrder);
+router.post("/cod", identifyCart, createCODOrder);
 router.put("/:id", isAuthenticated, isAdmin, updateOrderStatus);
 
 export default router;
