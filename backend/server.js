4c2fa1dd5a65bebdebb5627567433c74b6c716dd
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-03 16:15:53 +0500

    allowed requests from all origins

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ import orderRoute from './routes/orderRoute.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 const app=express();
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/user',userRoute);
