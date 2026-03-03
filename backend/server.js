33070deaa825c2eee0fd1206c9f50115b846f923
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-03 16:22:11 +0500

    fixed bug in frontend url

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 const app=express();
 app.use(cors({
-    origin: FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     credentials: true 
 }));
 app.use(express.json());
