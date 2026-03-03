90ca284ae5dbaeb9dc577fa464c2b87dc74796a8
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-03 17:48:13 +0500

    updated UI

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -69,7 +69,7 @@ const Cart = () => {
     loadCart()
   }, [dispatch])
   return (
-    <div className="pt-20 bg-gray-50 min-h-screen">
+    <div className="pt-2 bg-gray-50 min-h-screen">
       {cart?.items?.length > 0 ?
         <div className="max-w-7xl mx-auto">
           <div className="max-w-7xl mx-auto flex flex-col lg:flex-row gap-7 px-4">
@@ -81,7 +81,7 @@ const Cart = () => {
                     <div className="flex flex-col gap-5 flex-1">
                       {cart?.items?.map((product, index) => {
                         return (
-                          <Card key={index} className="p-4 shadow-sm hover:shadow-md transition rounded-xl">
+                          <Card key={index} className="p-4 shadow-sm border-1 border-gray-300 hover:shadow-md transition">
                             <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-6">
 
                               {/* Product Info */}
@@ -89,7 +89,7 @@ const Cart = () => {
                                 <img
                                   src={product?.productId?.productImg?.[0]?.url || userLogo}
                                   alt={product?.productId?.productName || "Product image"}
-                                  className="w-24 h-24 object-cover rounded-lg border"
+                                  className="w-24 h-24 object-cover border"
                                 />
 
                                 <div className="w-full sm:w-[280px] space-y-1">
@@ -113,11 +113,11 @@ const Cart = () => {
 
                               {/* Total Price */}
                               <p className="font-semibold text-gray-800">
-                                PKR {product?.productId?.productPrice * product?.quantity}
+                                Rs. {product?.productId?.productPrice * product?.quantity}
                               </p>
 
                               {/* Remove */}
-                              <p onClick={() => handleRemove(product?.productId?._id)} className="flex text-red-500 items-center gap-1 cursor-pointer hover:text-red-600 transition">
+                              <p onClick={() => handleRemove(product?.productId?._id)} className="flex text-gray-500 items-center gap-1 cursor-pointer hover:text-black transition">
                                 <Trash className="w-4 h-4" />
                                 Remove
                               </p>
@@ -128,37 +128,33 @@ const Cart = () => {
                         );
                       })}
                     </div>
-                    <Card className="w-full lg:w-[400px] mb-4">
+                    <Card className="w-full lg:w-[400px] mb-4 border border-gray-300">
                       <CardHeader>
                         <CardTitle>Order Summary</CardTitle>
                       </CardHeader>
                       <CardContent className="space-y-4">
                         <div className="flex justify-between">
                           <span>Shipping</span>
-                          <span>Rs{shipping}</span>
+                          <span>Rs. {shipping}</span>
                         </div>
                         <div className="flex justify-between">
                           <span>Tax(5%)</span>
-                          <span>Rs{tax}</span>
+                          <span>Rs. {tax}</span>
                         </div>
                         <Separator />
                         <div className='flex justify-between font-bold text-lg'>
                           <span>Total</span>
-                          <span>Rs{total}</span>
+                          <span>Rs. {total}</span>
                         </div>
                         <div className='space-y-3 pt-4'>
                           <div className='flex space-x-2'>
                             <Input placeholder="Promo Code" />
-                            <Button variant='outline'>Apply</Button>
+                            <Button variant='outline' className='bg-white text-black hover:bg-black hover:text-white border border-gray-300'>Apply</Button>
                           </div>
-                          <Button onClick={()=>navigate('/address')} className='w-full bg-pink-600'>PLACE ORDER</Button>
-                          <Button variant='outline' className='w-full bg-transparent'>
+                          <Button onClick={()=>navigate('/address')} className='w-full text-white hover:bg-white hover:text-black border border-gray-300'>PLACE ORDER</Button>
+                          <Button variant='outline' className='w-full bg-white text-black hover:bg-black hover:text-white border border-gray-300'>
                             <Link to="/products">Continue Shopping</Link>
                           </Button>
-                        </div>
-                        <div className='text-sm text-muted-foreground pt-4'>
-                          <p>* Free shipping on orders over 299</p>
-                          <p>* 30-days return policy</p>         
                         </div>
                       </CardContent>
                     </Card>
@@ -170,13 +166,13 @@ const Cart = () => {
         </div>
         : <div className="flex flex-col items-center justify-center min-h-[60vh] p-6 text-center">
           {/* Icon */}
-          <div className="bg-pink-100 p-6 rounded-full">
-            <ShoppingCart className="w-16 h-16 text-pink-600" />
+          <div className="bg-gray-200 p-6 rounded-full">
+            <ShoppingCart className="w-16 h-16 text-black" />
           </div>
           {/* title */}
           <h2 className="mt-6 text-2xl font-bold text-gray-800">Your Cart is Empty</h2>
           <p className="mt-2 text-gray-600">Looks like you haven't added anything to your cart yet</p>
-          <Button onClick={() => navigate('/products')} className='mt-6 cursor-pointer bg-pink-600 text-white py-3 hover:bg-pink-700'>Shop now</Button>
+          <Button onClick={() => navigate('/products')} className='mt-6 cursor-pointer bg-white text-black py-3 hover:bg-black hover:text-white border border-black'>Shop now</Button>
         </div>
       }
     </div >
