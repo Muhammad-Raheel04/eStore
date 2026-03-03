3883cdebc11903ad3001f3f9b15108575d4577cd
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-03 15:00:43 +0500

    improved UI of Top filter Bar

--- frontend/src/components/TopFilterBar.jsx
+++ frontend/src/components/TopFilterBar.jsx
@@ -3,16 +3,22 @@ import {
     Select,
     SelectContent,
     SelectItem,
+    SelectGroup,
     SelectTrigger,
     SelectValue,
 } from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
+import { Input } from "./ui/input";
 
 const TopFilterBar = ({
     category,
     setCategory,
     brand,
     setBrand,
+    sortOrder,
+    setSortOrder,
+    search,
+    setSearch, 
     priceRange,
     setPriceRange,
     allProducts,
@@ -20,19 +26,24 @@ const TopFilterBar = ({
 }) => {
     const categories = ["All", ...new Set(allProducts.map(p => p.category))];
     const brands = ["All", ...new Set(allProducts.map(p => p.brand))];
-
     return (
         <div className="w-full border-y bg-white py-4 px-4 flex flex-wrap gap-4 items-center justify-between">
 
             <div className="flex flex-wrap gap-4">
 
+                <Input
+                    type="text"
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder="Search..."
+                    className="w-[220px] bg-white rounded-md border-gray-200 border " />
                 {/* Category */}
                 <Select onValueChange={setCategory} value={category}>
                     <SelectTrigger className="w-[160px]">
                         <SelectValue placeholder="CATEGORY" />
                     </SelectTrigger>
                     <SelectContent>
-                      
+
                         {categories.map((item, index) => {
                             const label = item === "All" ? "Categories" : item;
                             return (
@@ -85,10 +96,24 @@ const TopFilterBar = ({
             {/* Reset Button */}
             <Button
                 onClick={resetFilters}
-                className="bg-pink-600 text-white"
+                className="bg-white  !p-2 text-black !font-normal border border-gray-200 rounded-md hover:text-white"
             >
                 Reset Filter
             </Button>
+            {/* Right Side - Sort */}
+            <div>
+                <Select onValueChange={(value) => setSortOrder(value)} value={sortOrder}>
+                    <SelectTrigger className="w-[200px]">
+                        <SelectValue placeholder="Sort by price" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectItem value="lowToHigh">Price: Low to High</SelectItem>
+                            <SelectItem value="highToLow">Price: High to Low</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
         </div>
     );
 };
