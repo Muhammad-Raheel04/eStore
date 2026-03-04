7207e4718094fbc935afe9b1715f6c2cfc243860
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-04 15:23:39 +0500

    added  categoriesMap and types constants for dynamic product filtering

---
+++ frontend/src/constants/categories.js
@@ -0,0 +1,8 @@
+export const categoriesMap = {
+  men: ["shirts", "pants", "shoes", "watches"],
+  women: ["dresses", "heels", "bags", "jewelry"],
+  kids: ["toys", "school-wear", "kids-shoes"],
+  unisex: ["hoodies", "caps"],
+};
+
+export const types = ["men", "women", "kids", "unisex"];
