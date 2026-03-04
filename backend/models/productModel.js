5f28c450506276cd0e51f99dad15ff2f33f47a2b
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-04 15:17:21 +0500

    added product type field and category validation by type

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
+export const categoriesByType = {
+    men: ["shirts", "pants", "shoes", "watches"],
+    women: ["dresses", "heels", "bags", "jewelry"],
+    kids: ["toys", "school-wear", "kids-shoes"],
+    unisex: ["hoodies", "caps"]
+};
+
 const productSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
@@ -14,8 +21,24 @@ const productSchema = new mongoose.Schema({
         }
     ],
     productPrice: { type: Number },
-    category: { type: String },
-    brand: { type: String }
+    category: { 
+        type: String,
+        required: true,
+        validate: {
+            validator: function (val) {
+                if (!this.type) return true;
+                const allowed = categoriesByType[this.type] || [];
+                return allowed.includes(val);
+            },
+            message: props => `Invalid category '${props.value}' for type`
+        }
+    },
+    type: {
+        type: String,
+        enum: ["men", "women", "kids", "unisex"],
+        required: true,
+        default: "unisex"
+    }
 }, {
     timestamps: true
 });
