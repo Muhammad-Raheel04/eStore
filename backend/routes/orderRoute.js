192d81b22d90a082ff9ca5b9936a07dd0215a513
Muhammad Raheel <raheel31102004@gmail.com>
2026-03-17 14:07:33 +0500

    add admin-protected /sales route to fetch sales analytics data.

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middleware/isAuthenticated.js";
-import { createCODOrder, getAllOrders, updateOrderStatus } from "../controllers/orderController.js";
+import { createCODOrder, getAllOrders, getSalesData, updateOrderStatus } from "../controllers/orderController.js";
 import { identifyCart } from "../middleware/cartIdentity.js";
 const router = express.Router();
 
 router.get('/all-orders',isAuthenticated,isAdmin,getAllOrders);
 router.post("/cod", identifyCart, createCODOrder);
 router.put("/:id", isAuthenticated, isAdmin, updateOrderStatus);
+router.get("/sales",isAuthenticated,isAdmin,getSalesData);
 
 export default router;
