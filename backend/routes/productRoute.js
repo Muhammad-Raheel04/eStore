14b88c7ab6c7002f189ed0e0d3c3547700a54ade
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-02 19:25:03 +0500

    feat: add featured product routes and endpoints

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addProduct ,deleteProduct,getAllProduct, updateProduct} from '../controllers/productController.js';
+import { addProduct ,deleteProduct,getAllProduct, updateProduct, toggleFeatured, getFeaturedProducts} from '../controllers/productController.js';
 import { isAdmin, isAuthenticated } from '../middleware/isAuthenticated.js';
 import { multipleUpload } from '../middleware/multer.js';
 
@@ -7,6 +7,8 @@ import { multipleUpload } from '../middleware/multer.js';
 const router = express.Router();
 router.post('/', isAuthenticated, multipleUpload,isAdmin, addProduct);
 router.get('/getallproducts',getAllProduct);
+router.get('/featured', getFeaturedProducts);
 router.delete('/delete/:productId', isAuthenticated, isAdmin, deleteProduct);
 router.put("/update/:productId", isAuthenticated, isAdmin, multipleUpload, updateProduct);
+router.patch("/featured/:productId", isAuthenticated, isAdmin, toggleFeatured);
 export default router;
