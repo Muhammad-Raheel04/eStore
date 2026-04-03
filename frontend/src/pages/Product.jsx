32a57f5e0027ed0a88439f38e4a306911809dee3
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-03 13:09:18 +0500

    update UI of product component

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -197,8 +197,8 @@ const Products = () => {
 
 
                 <div className="flex-1">
-                    {/* Products */}
-                    <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-2'>
+                    {/* Products Grid */}
+                    <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-12 sm:gap-x-6 sm:gap-y-16'>
                         {products.filter(Boolean).map((product) => (
                             <ProductCard
                                 key={product._id}
