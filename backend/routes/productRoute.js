4f9edd8386596c9660ae562a2c712190f7a7dc7e
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-03 11:31:37 +0500

    integrate get product by id route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addProduct ,deleteProduct,getAllProduct, updateProduct, toggleFeatured, getFeaturedProducts} from '../controllers/productController.js';
+import { addProduct ,deleteProduct,getAllProduct, updateProduct, toggleFeatured, getFeaturedProducts, getProductById} from '../controllers/productController.js';
 import { isAdmin, isAuthenticated } from '../middleware/isAuthenticated.js';
 import { multipleUpload } from '../middleware/multer.js';
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post('/', isAuthenticated, multipleUpload,isAdmin, addProduct);
 router.get('/getallproducts',getAllProduct);
 router.get('/featured', getFeaturedProducts);
+router.get('/:productId', getProductById);
 router.delete('/delete/:productId', isAuthenticated, isAdmin, deleteProduct);
 router.put("/update/:productId", isAuthenticated, isAdmin, multipleUpload, updateProduct);
 router.patch("/featured/:productId", isAuthenticated, isAdmin, toggleFeatured);
