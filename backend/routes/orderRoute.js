ec0f804c0564abdf8e0a3f46b5ec7482bfb02209
Muhammad Raheel <raheel31102004@gmail.com>
2026-04-23 09:41:15 +0500

    feat(orderRoute): integrate getMyOrder controller at route my-orders to fetch user orders

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { isAdmin, isAuthenticated } from "../middleware/isAuthenticated.js";
-import { createCODOrder, getAllOrders, getSalesData, updateOrderStatus } from "../controllers/orderController.js";
+import { createCODOrder, getAllOrders, getMyOrder, getSalesData, updateOrderStatus } from "../controllers/orderController.js";
 import { identifyCart } from "../middleware/cartIdentity.js";
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.get('/all-orders',isAuthenticated,isAdmin,getAllOrders);
 router.post("/cod", identifyCart, createCODOrder);
 router.put("/:id", isAuthenticated, isAdmin, updateOrderStatus);
 router.get("/sales",isAuthenticated,isAdmin,getSalesData);
+router.get('/my-orders',isAuthenticated,getMyOrder);
 
 export default router;
